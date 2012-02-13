e4de200a4f4d9b5fc1031c62820e0df60d617951
Ian Preston <ian@ian-preston.com>
2012-02-12 20:00:51 -0700

    re-arrange generate.js to make more sense

--- generate.js
+++ generate.js
@@ -14,22 +14,6 @@ var fs = require('fs'),
 
 
 /**
- * Compiles a template and saves it to disk at config.webRoot + path. Expects that
- * the full path to file exists and is writable and that nothing bad ever
- * happens.
- *
- * path : The page's path relative to config.webRoot
- * template : A string containing the template contents
- * context : The template context
- */
-var saveTemplate = function(savePath, template, context) {
-    var hoganTemplate = hogan.compile(template);
-    var finalPageContent = hoganTemplate.render(context);
-
-    fs.writeSync(fs.openSync(path.join(config.webRoot, savePath), 'w'), finalPageContent);
-};
-
-/**
  * Generates the entire static site. Generates each static page and moves them
  * to webroot, copies all statics to webroot, and does everything else to compile
  * the entire site into webroot.
@@ -49,6 +33,34 @@ exports.generateSite = function() {
             generateStaticMediaFile(f._id, config.webRoot);
         });
     });
+};
+
+/**
+ * The inverse of generatePage. Takes the ObjectId of a Page and
+ * deletes the generated static file for that page from disk.
+ */
+exports.deletePage = function(pageId, deletedCallback) {
+    Page.findOne({'_id': pageId}, function(err, page) {
+        fs.unlink(path.join(config.webRoot, page.path), function(err) {
+            deletedCallback();
+        });
+    });
+};
+
+/**
+ * Compiles a template and saves it to disk at config.webRoot + path. Expects that
+ * the full path to file exists and is writable and that nothing bad ever
+ * happens.
+ *
+ * path : The page's path relative to config.webRoot
+ * template : A string containing the template contents
+ * context : The template context
+ */
+var saveTemplate = function(savePath, template, context) {
+    var hoganTemplate = hogan.compile(template);
+    var finalPageContent = hoganTemplate.render(context);
+
+    fs.writeSync(fs.openSync(path.join(config.webRoot, savePath), 'w'), finalPageContent);
 };
 
 /**
@@ -89,19 +101,6 @@ var generateStaticMediaFile = function(fileId) {
                     console.log(err);
                 }
             });
-        });
-    });
-};
-         
-
-/**
- * The inverse of generatePage. Takes the ObjectId of a Page and
- * deletes the generated static file for that page from disk.
- */
-exports.deletePage = function(pageId, deletedCallback) {
-    Page.findOne({'_id': pageId}, function(err, page) {
-        fs.unlink(path.join(config.webRoot, page.path), function(err) {
-            deletedCallback();
         });
     });
 };
