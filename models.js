ca06f5babef221404fca25538a2463a40f1331e9
Ian Preston <ian@ian-preston.com>
2012-03-03 14:28:17 -0700

    prevent page paths from being directories

--- models.js
+++ models.js
@@ -11,7 +11,10 @@ var PageSchema = new mongoose.Schema({
     // a page's .path is '/index.html', the page will be compiled and saved to
     // '/webroot/index.html'
     path: { type: String,
-            validate: [function(p) { return p != '/'; }, 'Page must have a path'],
+            // Validate that the path does not end with '/'. Since the set method will prepend '/' to
+            // the path, this will return false if the path is empty as well as if it is a directory.
+            validate: [function(p) { return p.charAt(p.length-1) != '/'; }, 'Page must have a path that is not a directory'],
+
             set: function(p) { return path.normalize('/' + p); } },
 
     // The Template that this page belongs to
